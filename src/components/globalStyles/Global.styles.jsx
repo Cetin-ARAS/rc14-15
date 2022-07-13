9d6f5a37c3e2b0c17a65ae43ebad58c21443c212
ÇETİN <cetinnnarasss@gmail.com>
2022-07-13 21:28:40 +0300

    .

---
+++ src/components/globalStyles/Global.styles.jsx
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin : 0;
+    fonts-family: 'Girassol', cursive;
+}
+body{
+    fontsize: 1.5rem;
+}
