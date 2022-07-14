75a4038dd665525e47a0525dae13d1f56666022e
ÇETİN <cetinnnarasss@gmail.com>
2022-07-14 22:27:01 +0300

    cont.

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,21 @@
-import CardsStyle from './Cards.style';
+import CardsStyle, { Card, MainContainer } from './Cards.style';
 
-const Cards = () => {
-  return <div>Cards</div>;
+const Cards = ({ recipes }) => {
+  return(
+
+   <MainContainer wrap="wrap">
+
+    {recipes.map(({recipe}, index)=>(
+    
+    <Card>
+      <Header>{recipe.label}</Header>    
+      <Image src={recipe.image || defaultImage} /> 
+      <Button onClick={()=> navigate('detail', { state: recipe }) </Card>>View More</Button>
+
+    </Card>
+))}
+  </MainContainer>;
+  );
 };
 
 export default Cards;
